ce062ad8e57aadca21d5df5ab63ea1185d9aef62
RaspVor <58893077+RaspVor@users.noreply.github.com>
2019-12-26 16:28:52 +0100

    Create code2.py

---
+++ code2.py
@@ -0,0 +1,40 @@
+exec(open("./requirements.py").read())
+exec(open("./classes.py").read())
+exec(open("./functions.py").read())
+
+#Who is playing?
+nb_players = nb_players(3)
+nb_players.hmp()
+
+
+p1=Player("Francois", "Human", 1)
+p2=Player("Neuromancer", "Bot", 2)
+p3=Player("Shooter", "Bot", 3)
+
+
+
+#Shuffling the cards
+deck = cards_game()
+
+if nb_players.nb == 2:
+    deck.cards_list = shuffle_deck(deck.cards_list)[:-10] 
+else:
+    deck.cards_list = shuffle_deck(deck.cards_list)
+
+last_cards=np.array([])
+if len(deck.cards_list) % nb_players.nb != 0:
+    last_cards = deck.cards_list[-1]
+    deck.cards_list = deck.cards_list[:-1]
+
+#Distribution of the cards
+for i in Player.players_list:
+    i.myname()
+    
+    if i.player_number < nb_players.nb:
+        i.cards = deck.cards_list.reshape(len(deck.cards_list)//nb_players.nb,nb_players.nb)[:,i.player_number-1]
+        print(i.cards)
+    else:
+        i.cards = deck.cards_list.reshape(len(deck.cards_list)//nb_players.nb,nb_players.nb)[:,i.player_number-1]
+        print(i.cards)
+        
+        
