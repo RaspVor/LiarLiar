368bc4169f897b4486cf79b38ae53fd74d1ad993
RaspVor <58893077+RaspVor@users.noreply.github.com>
2019-12-26 01:19:26 +0100

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,11 +1,23 @@
-#Functions
+#Initialization
 
-class Player:
-  def __init__(self, name):
+class Player_Human:
+  def __init__(self, name, cards, cave):
     self.name = name
 
   def myname(self):
-    print("Hello my name is " + self.name)
+    print("Hello my name is " + self.name + ".")
 
-p1=Player("Francois")
+
+ 
+class Player_IA:
+  def __init__(self, name, cards, cave):
+    self.name = name
+
+  def myname(self):
+    print("Hello my name is " + self.name + ". I'm a robot.")
+
+p1=Player_Human("Francois")
 p1.myname()
+
+p2=Player_IA("Neuromancer")
+p2.myname()
