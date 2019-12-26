9c7b1d29e9e0370716a002db009574626936a196
RaspVor <58893077+RaspVor@users.noreply.github.com>
2019-12-26 18:58:00 +0100

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,23 +1,11 @@
-import PySimpleGUI as sg
+class nb_players:
+    def __init__(self,nb):
+        self.nb=nb
+        
+    def hmp(self):
+        print("There are " + str(self.nb) + " players. Prepare for the battle!")
+        
+nb_players = nb_players(3)
 
-sg.change_look_and_feel('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
-layout2 = [  [sg.Text('My Lord,')],
-            [sg.Text('What is your summoner name?'), sg.InputText()],
-            [sg.Text('Are your a human?'), sg.Radio('I am a human!     ', "RADIO1"), sg.Radio('I am a ROBOT!    ', "RADIO1", default=True)],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
-
-# Create the Window
-window = sg.Window('Introduce yourself!', layout2)
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
-    if event in (None, 'Cancel'):   # if user closes window or clicks cancel
-        break
-    robot_statut = "as an IA!"
-    if values[1] == True:
-        robot_statut = "as a humanoid!"
-    print(values[0], "has joined the game", robot_statut)
-    break
-
-window.close()
+for i in range(nb_players.nb):
+    print(i)
