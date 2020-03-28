21fac03a0617fd9f376a366277d5d556448df557
RaspVor <58893077+RaspVor@users.noreply.github.com>
2020-03-28 19:36:34 +0100

    Update 1_Preparation.py

--- 1_Preparation.py
+++ 1_Preparation.py
@@ -28,9 +28,9 @@ for i in Player.players_list:
     i.myname()
     
     if i.player_number < nb_players.nb:
-        i.cards = deck.cards_list.reshape(len(deck.cards_list)//nb_players.nb,nb_players.nb)[:,i.player_number]
+        i.cards = np.sort(deck.cards_list.reshape(len(deck.cards_list)//nb_players.nb,nb_players.nb)[:,i.player_number])
     else:
-        i.cards = np.append(deck.cards_list.reshape(len(deck.cards_list)//nb_players.nb,nb_players.nb)[:,i.player_number],deck.last_cards)
+        i.cards = np.sort(np.append(deck.cards_list.reshape(len(deck.cards_list)//nb_players.nb,nb_players.nb)[:,i.player_number],deck.last_cards))
 
     print(i.cards)
         
