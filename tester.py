8d7a217708c9d346fd2ca81e26b7e19af381567f
RaspVor <58893077+RaspVor@users.noreply.github.com>
2020-03-28 19:36:27 +0100

    Update tester.py

--- tester.py
+++ tester.py
@@ -9,3 +9,39 @@ nb_players = nb_players(3)
 
 for i in range(nb_players.nb):
     print(i)
+
+nb_players.hmp()
+
+
+
+
+class Player:
+    players_list = []
+    
+    def __init__(self, name = "Anonymous", IA = "Bot", player_number = int, cards = np.array([]), cave = np.array([])):
+        self.name = name
+        self.IA = IA
+        self.player_number = player_number
+        self.cards = cards
+        self.cave = cave
+        self.players_list.append(self)
+    
+    
+    def myname(self):
+        if self.IA == "Bot" :
+            print("Hello my name is " + self.name + ". I'm a robot.")
+        else:
+            print("Hello my name is " + self.name + ".")
+
+
+nb_players.hmp()
+
+class game_turn_nb:
+    def __init__(self,num = 1):
+        self.num=num
+        
+    def which_turn(self):
+        print("It's turn number " + str(self.num) + "!")
+        
+game_turn_nb = game_turn_nb()        
+game_turn_nb.which_turn()
