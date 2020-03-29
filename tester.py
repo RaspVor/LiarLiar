23c04bf6b1f8d5a3737718db19d9bd5137f7f4d2
RaspVor <58893077+RaspVor@users.noreply.github.com>
2020-03-29 19:32:54 +0200

    Update tester.py

--- tester.py
+++ tester.py
@@ -28,3 +28,11 @@ class game_turn_nb:
         
 game_turn_nb = game_turn_nb(1,dice.winner,Player.players_list)  
 game_turn_nb.which_turn()
+
+
+column_names = ["turn", "picker_name", "caller_name", "picker_cards", "caller_choose", "winner"]
+df = pd.DataFrame(columns = column_names)
+
+df = pd.DataFrame([[1,2,3,4,5,6]],columns = column_names)
+df = pd.DataFrame([[1,2,3,4,5,6]],columns = column_names)
+df.head()
