dced18219ba2ba728ceb07b1c98d1011afc06c13
RaspVor <58893077+RaspVor@users.noreply.github.com>
2020-04-04 00:11:03 +0200

    Update 1_Preparation.py

--- 1_Preparation.py
+++ 1_Preparation.py
@@ -42,3 +42,38 @@ dice.who_start()
 
 #Initialize Counter
 counter_turnZ = counter_turn(1, dice.winner)
+
+
+#THE GAME
+turn_startZ = turn_start(1,counter_turnZ.winner,Player.players_list)  
+turn_startZ.which_turn()
+
+
+##pick & announce
+if Player.players_list[counter_turnZ.winner].IA == "Bot" :
+    card_picked = one_turn_IA_picked(counter_turnZ.counter, counter_turnZ.winner)
+    card_announced = one_turn_IA_announce(counter_turnZ.counter, counter_turnZ.winner)
+else:
+    card_picked = one_turn_human_picked(counter_turnZ.counter, counter_turnZ.winner)
+    card_announced = one_turn_human_announce(counter_turnZ.counter, counter_turnZ.winner) 
+    
+turn_pickerZ = turn_picker(counter_turnZ.winner,Player.players_list, card_picked, card_announced)  
+turn_pickerZ.turn_picker_call()
+    
+
+
+##guess
+if Player.players_list[(counter_turnZ.winner+1)%len(Player.players_list)].IA == "Bot" :
+    choice_made = one_turn_IA_guess(counter_turnZ.counter, (counter_turnZ.winner+1)%len(Player.players_list))
+    
+else:
+    choice_made = one_turn_human_guess(counter_turnZ.counter, (counter_turnZ.winner+1)%len(Player.players_list))
+
+
+turn_callerZ = turn_caller((counter_turnZ.winner+1)%len(Player.players_list),Player.players_list, choice_made)  
+turn_callerZ.turn_caller_call()    
+    
+
+##wicher
+who_wonZ = who_won(counter_turnZ.winner,Player.players_list,card_picked, card_announced,choice_made) 
+who_wonZ.tellwhowon()    
