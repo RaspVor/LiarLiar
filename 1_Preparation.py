feb626608acdadfb9806ee716eb4800ebd737cfb
RaspVor <58893077+RaspVor@users.noreply.github.com>
2020-04-04 11:04:42 +0200

    Update 1_Preparation.py

--- 1_Preparation.py
+++ 1_Preparation.py
@@ -51,58 +51,63 @@ counter_turnZ = counter_turn(1, dice.winner)
 column_names = ["turn", "picker_number", "caller_number", "picker_cards", "picker_call", "caller_choose", "winner", "loser"]
 myMatrix = pd.DataFrame(columns = column_names)
 
-#THE GAME
-turn_startZ = turn_start(1,counter_turnZ.winner,Player.players_list)  
-turn_startZ.which_turn()
-
-
-##pick & announce
-if Player.players_list[counter_turnZ.winner].IA == "Bot" :
-    card_picked = one_turn_IA_picked(counter_turnZ.counter, counter_turnZ.winner)
-    card_announced = one_turn_IA_announce(counter_turnZ.counter, counter_turnZ.winner)
-else:
-    card_picked = one_turn_human_picked(counter_turnZ.counter, counter_turnZ.winner)
-    card_announced = one_turn_human_announce(counter_turnZ.counter, counter_turnZ.winner) 
+for i in range(1,10):
     
-turn_pickerZ = turn_picker(counter_turnZ.winner,Player.players_list, card_picked, card_announced)  
-turn_pickerZ.turn_picker_call()
+    #THE GAME
+    turn_startZ = turn_start(i,counter_turnZ.winner,Player.players_list)  
+    turn_startZ.which_turn()
     
-
-
-##guess
-if Player.players_list[(counter_turnZ.winner+1)%len(Player.players_list)].IA == "Bot" :
-    choice_made = one_turn_IA_guess(counter_turnZ.counter, (counter_turnZ.winner+1)%len(Player.players_list))
     
-else:
-    choice_made = one_turn_human_guess(counter_turnZ.counter, (counter_turnZ.winner+1)%len(Player.players_list))
-
-
-turn_callerZ = turn_caller((counter_turnZ.winner+1)%len(Player.players_list),Player.players_list, choice_made)  
-turn_callerZ.turn_caller_call()    
+    ##pick & announce
+    if Player.players_list[counter_turnZ.winner].IA == "Bot" :
+        card_picked = one_turn_IA_picked(counter_turnZ.counter, counter_turnZ.winner)
+        card_announced = one_turn_IA_announce(counter_turnZ.counter, counter_turnZ.winner)
+    else:
+        card_picked = one_turn_human_picked(counter_turnZ.counter, counter_turnZ.winner)
+        card_announced = one_turn_human_announce(counter_turnZ.counter, counter_turnZ.winner) 
+        
+    turn_pickerZ = turn_picker(counter_turnZ.winner,Player.players_list, card_picked, card_announced)  
+    turn_pickerZ.turn_picker_call()
+        
     
-
-##winner
-who_wonZ = who_won(counter_turnZ.winner,Player.players_list,card_picked, card_announced,choice_made) 
-who_wonZ.tellwhowon()    
-
-
-##players Update
-topage_cards = np.where(Player.players_list[counter_turnZ.winner].cards[0] == card_picked)[0][0]
-topage_cave = np.where(Player.players_list[who_wonZ.return_loser()].cave[0] == card_picked)[0][0]
-
-Player.players_list[counter_turnZ.winner].cards[1][topage_cards] = Player.players_list[counter_turnZ.winner].cards[1][topage_cards] -1
-Player.players_list[who_wonZ.return_loser()].cave[1][topage_cave] = Player.players_list[who_wonZ.return_loser()].cave[1][topage_cave] + 1
-
-for i in Player.players_list:
-    i.myname()
     
-    print(i.cards)
-    print(i.cave)
+    ##guess
+    if Player.players_list[(counter_turnZ.winner+1)%len(Player.players_list)].IA == "Bot" :
+        choice_made = one_turn_IA_guess(counter_turnZ.counter, (counter_turnZ.winner+1)%len(Player.players_list))
+        
+    else:
+        choice_made = one_turn_human_guess(counter_turnZ.counter, (counter_turnZ.winner+1)%len(Player.players_list))
+    
+    
+    turn_callerZ = turn_caller((counter_turnZ.winner+1)%len(Player.players_list),Player.players_list, choice_made)  
+    turn_callerZ.turn_caller_call()    
+        
+    
+    ##winner
+    who_wonZ = who_won(counter_turnZ.winner,Player.players_list,card_picked, card_announced,choice_made) 
+    who_wonZ.tellwhowon()    
+    
+    show_winner(counter_turnZ.counter, who_wonZ.showwhowon())
+    
+    ##players Update
+    topage_cards = np.where(Player.players_list[counter_turnZ.winner].cards[0] == card_picked)[0][0]
+    topage_cave = np.where(Player.players_list[who_wonZ.return_loser()].cave[0] == card_picked)[0][0]
+    
+    Player.players_list[counter_turnZ.winner].cards[1][topage_cards] = Player.players_list[counter_turnZ.winner].cards[1][topage_cards] -1
+    Player.players_list[who_wonZ.return_loser()].cave[1][topage_cave] = Player.players_list[who_wonZ.return_loser()].cave[1][topage_cave] + 1
+    
+    for i in Player.players_list:
+        i.myname()
+        
+        print(i.cards)
+        print(i.cave)
+    
+    ##Data Update
+    myMatrix_temp = pd.DataFrame([[turn_startZ.turn_num,counter_turnZ.winner,(counter_turnZ.winner+1)%len(Player.players_list),card_picked, card_announced,choice_made,who_wonZ.return_winner(), who_wonZ.return_loser()]],columns = column_names)
+    myMatrix = myMatrix.append(myMatrix_temp, ignore_index=True)   
+    
+    
+    ##Next Turn
+    counter_turnZ = counter_turn(counter_turnZ.counter+1, who_wonZ.return_loser())
 
-##Data Update
-myMatrix_temp = pd.DataFrame([[turn_startZ.turn_num,counter_turnZ.winner,(counter_turnZ.winner+1)%len(Player.players_list),card_picked, card_announced,choice_made,who_wonZ.return_winner(), who_wonZ.return_loser()]],columns = column_names)
-myMatrix = myMatrix.append(myMatrix_temp, ignore_index=True)   
 
-
-##Next Turn
-#counter_turnZ = counter_turn(counter_turnZ.counter+1, who_wonZ.return_loser())
